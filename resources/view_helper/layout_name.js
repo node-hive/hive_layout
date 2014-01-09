c8326a8b7d3eaec7e3557c62723520f6a8407c74
Dave Edelhart <dave@wonderlandlabs.com>
2014-01-09 10:50:39 -0800

    Allowing for layout_name in frame; code cleanup

--- resources/view_helper/layout_name.js
+++ resources/view_helper/layout_name.js
@@ -1,24 +1,30 @@
 var _ = require('underscore');
 var util = require('util');
 var path = require('path');
-var _DEBUG = false;
+var DEBUG = true;
 
 module.exports = function (apiary, cb) {
 
-	var helper = {
-		name: 'layout_name',
+    var helper = {
+        name: 'layout_name',
 
-		test: function (ctx, output) {
-			return ctx.$action.has_config('layout_name') || ctx.$action.get_config('hive').has_config('layout_name');
-		},
+        test: function (ctx, output) {
+            var action = ctx.$action;
+            var hive = action.get_config('hive');
+            var frame = hive.get_config('frame');
+            return action.has_config('layout_name') || hive.has_config('layout_name') || frame.has_config('layout_name');
+        },
 
-		weight:-100,
+        weight: -100,
 
-		respond: function (ctx, output, cb) {
-			output.layout_name = ctx.$action.get_config('layout_name') || ctx.$action.get_config('hive').get_config('layout_name');
-		    cb(null, ctx, output);
-		}
-	};
+        respond: function (ctx, output, cb) {
+            var action = ctx.$action;
+            var hive = action.get_config('hive');
+            var frame = hive.get_config('frame');
+            output.layout_name = action.get_config('layout_name') || hive.get_config('layout_name') || frame.get_config('layout_name');
+            cb(null, ctx, output);
+        }
+    };
 
-	cb(null, helper);
+    cb(null, helper);
 };
